0f61570ebf2019a8b882e7e935ad45ee9e3babfc
Gal Szkolnik <code@lockszmith.com>
2020-01-10 13:37:18 -0500

    Cleanup + new feature
    🎨 Cleanup formatting / indentation.
    💄 User name is now a paramter, with a clear exception when it's missing.
    ✨ Added Site selection paramater, default behaves like before.
    ✨ Added dynamic validation based on array. (Still need to add synamic parameter AutoFill)
    💄 Changed output to be more POSH style, only results will be in Output, while anything else will go to host / error / verbose or warning. (This is to adher to the POSH philosophy of 'everythin is an object')
    💄 Output will be service name (based on preset array). URL will be displayed only for custom addresses.

--- powersosh.ps1
+++ powersosh.ps1
@@ -1,89 +1,116 @@
-﻿<#
-	.SYNOPSIS
-		OSINT Tool
-
-	.DESCRIPTION
-		Find usernames on the web using PowerShell
-
-	.NOTES
-		Aurthor: https://securethelogs.com
+<#
+.SYNOPSIS
+    OSINT Tool
+.DESCRIPTION
+    Find usernames on the web using PowerShell
+.EXAMPLE
+    powersosh -UserHandle Username
+.EXAMPLE
+    powersosh -UserHandle Username -Sites Twitter,'https://some.other.site/user/#u'
+.INPUTS
+    A userhandle and an optional list of services / URLs
+.OUTPUTS
+    List of service names / URLs that were validated.
+.NOTES
+    Aurthor: https://securethelogs.com
 		
-		Idea: This script can build and build and build.........
+    Idea: This script can build and build and build.........
+.ROLE
+    Any role with web access can run this cmdlet
+.FUNCTIONALITY
+    The function builds a list of URLs (known or custom) and tests
+    access to them, The result will be a list of services (for known)
+    or URLs (for unkown) where the user has been found.
+
+    Verbose output will include the services that were not found.
 #>
-
-
 function powersosh {
-
-Write-Output ""
-Write-Output "__________                                             .__     "
-Write-Output "\______   \______  _  __ ___________  __________  _____|  |__  "
-Write-Output " |     ___/  _ \ \/ \/ // __ \_  __ \/  ___/  _ \/  ___/  |  \ "
-Write-Output " |    |  (  <_> )     /\  ___/|  | \/\___ (  <_> )___ \|   Y  \"
-Write-Output " |____|   \____/ \/\_/  \___  >__|  /____  >____/____  >___|  /"
-Write-Output "                            \/           \/          \/     \/ "
-Write-Output ""
-Write-Output "Creator: https://securethelogs.com / @securethelogs"
-Write-Output ""
-
-
-
-#Get the username
-
-$userhandle = Read-Host -Prompt "Enter A Username: "
-
-$myArray = @(
-"https://twitter.com/$userhandle",
-"https://www.instagram.com/$userhandle/",
-"https://ws2.kik.com/user/$userhandle/",
-"https://medium.com/@$userhandle",
-"https://pastebin.com/u/$userhandle/",
-"https://www.patreon.com/$userhandle/",
-"https://photobucket.com/user/$userhandle/library",
-"https://www.pinterest.com/$userhandle/",
-"https://myspace.com/$userhandle/",
-"https://www.reddit.com/user/$userhandle/"
-
+param(
+    # The handle/name for the user you are searching for
+    [string]$UserHandle =
+        $(Throw "Must supply -UserHandle value,`nfor example:`n`t"+
+            "powersosh UserName`nor`n`tpowersosh -UserHandle UserName.`n`n"+
+            "For more information run Get-Help powersosh`n`n"
+        ),
+    # List of sites and/or custom URLs to search. URLs must contain
+    # the #u placeholder which will be replaced by the userhandle.
+    [ValidateScript({Test-PowerSoshSites $_})]
+    [string[]]$Sites
 )
 
+    Write-Host ""
+    Write-Host "__________                                             .__     "
+    Write-Host "\______   \______  _  __ ___________  __________  _____|  |__  "
+    Write-Host " |     ___/  _ \ \/ \/ // __ \_  __ \/  ___/  _ \/  ___/  |  \ "
+    Write-Host " |    |  (  <_> )     /\  ___/|  | \/\___ (  <_> )___ \|   Y  \"
+    Write-Host " |____|   \____/ \/\_/  \___  >__|  /____  >____/____  >___|  /"
+    Write-Host "                            \/           \/          \/     \/ "
+    Write-Host ""
+    Write-Host "Creator: https://securethelogs.com / @securethelogs"
+    Write-Host ""
 
-Write-Output "`n"
-Write-Output "Running Checks.............."
-Write-Output "`n"
+    $local:testingSet = $( if( -not $Sites) {
+        $KnownSites.Values
+    } else {
+        $Sites | ForEach-Object { Test-PowerSoshSites $_ -PassThruOnly }
+    } ) | ForEach-Object { $_ -replace '#u',[system.uri]::EscapeUriString($UserHandle) }
 
-foreach ($i in $myArray) {
+    Write-Host "`n"
+    Write-Host "Running Checks..."
+    Write-Host "`n"
 
-try
+    foreach ($testUrl in $testingSet) {
+        try {
+            $response = Invoke-WebRequest -Uri "$testUrl" -ErrorAction Stop
+            $StatusCode = $Response.StatusCode
+        } catch {
+            $StatusCode = $_.Exception.Response.StatusCode.value__
+        }
 
-{
+        if ($StatusCode -eq "200") {
+            Write-Output Test-PowerSoshSites $testUrl -Reverse $UserHandle -PassThruOnly
+        }
 
-    $response = Invoke-WebRequest -Uri "$i" -ErrorAction Stop
-    $StatusCode = $Response.StatusCode
+        if ($StatusCode -eq "404") {
+            #Site Does Not Exist - Do Nothing
+        } else {
+            Write-Warning -Message "Error ($StatusCode) occured when querying $testUrl"
+        }
+    }
+
+    Write-Output "`n"
 }
-catch
-{
-    $StatusCode = $_.Exception.Response.StatusCode.value__
-}
 
+$script:KnownSites = [ordered]@{}
+# #u in a string indicates the user handle.
+$KnownSites['twitter'    ] = 'https://twitter.com/#u/'
+$KnownSites["instagram"  ] = "https://www.instagram.com/#u/"
+$KnownSites["kik"        ] = "https://ws2.kik.com/user/#u/"
+$KnownSites["medium"     ] = "https://medium.com/@#u"
+$KnownSites["pastebin"   ] = "https://pastebin.com/u/#u/"
+$KnownSites["patreon"    ] = "https://www.patreon.com/#u/"
+$KnownSites["photobucket"] = "https://photobucket.com/user/#u/library"
+$KnownSites["pinterest"  ] = "https://www.pinterest.com/#u/"
+$KnownSites["myspace"    ] = "https://myspace.com/#u/"
+$KnownSites["reddit"     ] = "https://www.reddit.com/user/#u/"
 
-if ($StatusCode -eq "200"){
+function Test-PowerSoshSites{
+param([string[]]$arg,[string]$Reverse,[switch]$PassThruOnly)
+    if( $reverse ) {
+        foreach( $site in $KnownSites.Keys ) {
+            $siteUri = $KnownSites[$site] -replace '#u',$Reverse
+            if( $siteUri -ieq $arg ) { if( $PassThruOnly ) { return $site } else { return $true } }
+        }
+        if( $PassThruOnly ) { return $arg } else { return $false }
+    }
+    if( $arg -in $KnownSites.Keys ) {
+        if( $PassThruOnly ) { return $KnownSites[$arg] } else { return $true }
+    }
+    Write-Verbose "($arg) is not predefined"
 
-Write-Output "Found one: $i"
+    if( [system.uri]::IsWellFormedUriString($arg,[System.UriKind]::Absolute) ) {
+        if( $arg -match '#u' ) { if( $PassThruOnly ) { return $arg } else { return $true } }
+    }
 
-}
-
-if ($StatusCode -eq "404"){
-
-#Site Does Not Exist - Do Nothing
-
-}
-
-else {
-
-#Do Nothing
-
-}
-
-}
-
-Write-Output "`n"
+    return $false
 }
